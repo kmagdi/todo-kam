8d2b1cb6a9fcd7253d3347285bbec000599c8983
KMagdi <46417302+kmagdi@users.noreply.github.com>
2022-05-21 21:03:39 +0200

    logout

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ import {onAuthStateChanged} from "firebase/auth";
 import {auth} from './firebaseApp'
 import NavigationBar from './components/NavigationBar.jsx';
 
+
 function App() {
   const [token, setToken] = useState();
 
   onAuthStateChanged(auth,(currentUser)=>setToken(currentUser))
-/*
+
   if(!token) {
     return <Login setToken={setToken} />
-  }*/
+  }
 
   return (
     <div className="wrapper">
